96b3ea1f1c0720cb3a45e885e14f0e07830900fe
ryan <rsgm123@gmail.com>
2015-10-19 21:10:13 -0500

    Finished Geodjango models and migrations

--- wyvern/chat/models.py
+++ wyvern/chat/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals, absolute_import
 
 from django.contrib.auth.models import AbstractUser
 from django.contrib.gis.db import models
+from django.contrib.gis.geos.point import Point
 from django.utils.encoding import python_2_unicode_compatible
 
 
@@ -17,10 +18,10 @@ class Category(models.Model):
 @python_2_unicode_compatible
 class Group(models.Model):
     category = models.ForeignKey(Category)
-    # id = models.CharField(max_length=30, primary_key=True)
+    id = models.CharField(max_length=30, primary_key=True)
     name = models.CharField(max_length=30)
-    description = models.CharField(max_length=255, default='')
-    # point = models.PointField()
+    description = models.CharField(max_length=255, default='Description')
+    point = models.PointField(default='POINT(0.0 0.0)')
 
     def __str__(self):
         return self.name
